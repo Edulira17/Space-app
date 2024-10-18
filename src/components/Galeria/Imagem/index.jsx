0c152af32ff7d1c25c82898ed1e681a7417befc2
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-10-18 14:43:23 -0400

    Utilizando useState para resgatar e renderizar dados de um arquivo json

---
+++ src/components/Galeria/Imagem/index.jsx
@@ -0,0 +1,18 @@
+
+const Imagem = () => {
+  return (
+    <figure>
+        <img src="" alt="" />
+        <figcaption>
+            <h3>Titulo</h3>
+            <footer>
+                <p>fonte</p>
+                <button>Favorito</button>
+                <button>Maximar</button>
+            </footer>
+        </figcaption>
+    </figure>
+  )
+}
+
+export default Imagem
